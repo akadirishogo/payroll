7ef5d687dccf012acf5da0e150e6b1e138ecb556
ShogoMark <trigmark87@gmail.com>
2025-03-19 08:16:59 +0100

    update

--- components/layouts/LandingLayout.tsx
+++ components/layouts/LandingLayout.tsx
@@ -19,23 +19,23 @@ export default function LandingLayout({ children }: LandingLayoutProps) {
 
   return (
     <div className="relative bg-backImage min-h-[100vh]">
-      <div className="pt-8 absolute inset-0 bg-primary bg-opacity-70">
+      <div className="pt-8 absolute inset-0 bg-primary bg-opacity-70 md:px-5">
         <div className="pl-8 xl:h-[35px] flex">
             <Image height={50} width={150} src={"/logo_white.png"} alt="logo" />
         </div>
         <div className="flex xl:pr-[20px] items-center justify-around h-[95%]">
-            <div className="xl:pl-[15px]">
-                <div className="2xl:pl-[5px]">
-                    <p className="font-boldItalic text-7xl 2xl:text-8xl inline-block mr-3 text-splash">Smart</p><p className="inline-block font-extraBold text-7xl 2xl:text-8xl text-white"> Payroll for</p>
-                    <p className="font-extraBold text-7xl 2xl:text-8xl text-white">Businesses</p>
+            <div className="md:w-[50%]">
+                <div className="2xl:pl-[5px] md:pl-[20px]">
+                    <p className="font-boldItalic text-7xl 2xl:text-8xl md:text-4xl inline-block mr-3 text-splash">Smart</p><p className="inline-block font-extraBold text-7xl 2xl:text-8xl md:text-6xl text-white"> Payroll for</p>
+                    <p className="font-extraBold text-7xl 2xl:text-8xl md:text-6xl text-white">Businesses</p>
                 </div>
-                <div className="w-[80%]">
+                <div className="w-[80%] md:pl-[20px]">
                     <p className="text-white text-[20px] 2xl:text-[30px] font-regular">Start managing your workforce Salaries with
                         automation, precision and security. </p>
                 </div>
                 <button
                     onClick={getStarted}
-                    className="bg-primary rounded-2xl text-white px-7 py-3 mt-10 font-semi tracking-widest xl:px-9 xl:py-4 2xl:text-[25px]">
+                    className="bg-primary rounded-2xl text-white px-7 py-3 mt-10 font-semi tracking-widest xl:px-9 xl:py-4 2xl:text-[25px] md:ml-[20px]">
                     Get Started
                 </button>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,6 +31,16 @@ export default {
         approved: '#13970D'
       },
 
+      screens: {
+        xs: "480px", // Custom extra small breakpoint
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1230px",
+        "2xl": "1536px",
+        "3xl": "1920px", // Custom extra large breakpoint
+      },
+
       backgroundImage: {
         'backImage': "url('/bgImage.png')"
       }
